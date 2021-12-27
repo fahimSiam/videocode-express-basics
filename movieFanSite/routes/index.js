39ef922053683642c18a1a46fadb947384bdf7a9
SiamUxd <siam@uxd.co.jp>
2021-12-27 17:01:56 +0600

    27/11

---
+++ movieFanSite/routes/index.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var router = express.Router();
+const request = require("request");
+
+const apiKey = "1fb720b97cc13e580c2c35e1138f90f8";
+const apiBaseUrl = "http://api.themoviedb.org/3";
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+const imageBaseUrl = "http://image.tmdb.org/t/p/w300";
+
+router.use((req, res, next) => {
+  res.locals.imageBaseUrl = imageBaseUrl;
+  next();
+});
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  request.get(nowPlayingUrl, (error, response, movieData) => {
+    console.log(error);
+    //console.log(response);
+    //console.log(movieData);
+    const parsedData = JSON.parse(movieData);
+    //console.log(parsedData);
+    //res.json(parsedData);
+    res.render("index", {
+      parsedData: parsedData.results,
+    });
+  });
+  //res.render("index", {});
+});
+
+module.exports = router;
